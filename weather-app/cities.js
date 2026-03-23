80d275bdd1663e619a89ab384a5c700208fdba98
polyzoidis <mpolyzoidis@york.citycollege.eu>
2026-03-23 16:45:00 +0200

    added getWeather to fetch data + renderCards() to render this data

---
+++ weather-app/cities.js
@@ -0,0 +1,40 @@
+const cities = {
+	thessaloniki: {
+		name: "Thessaloniki",
+		country: "Greece",
+		lat: 40.6401,
+		lon: 22.9444,
+	},
+	london: {
+		name: "London",
+		country: "UK",
+		lat: 51.5072,
+		lon: -0.1276,
+	},
+	newYork: {
+		name: "New York",
+		country: "USA",
+		lat: 40.7128,
+		lon: -74.006,
+	},
+	tokyo: {
+		name: "Tokyo",
+		country: "Japan",
+		lat: 35.6762,
+		lon: 139.6503,
+	},
+	sydney: {
+		name: "Sydney",
+		country: "Australia",
+		lat: -33.8688,
+		lon: 151.2093,
+	},
+	cairo: {
+		name: "Cairo",
+		country: "Egypt",
+		lat: 30.0444,
+		lon: 31.2357,
+	},
+}
+
+export default cities
--- weather-app/main.js
+++ weather-app/main.js
@@ -1,1 +1,48 @@
+import cities from "./cities"
 
+async function getWeather(city) {
+	try {
+		const response = await fetch(
+			`https://api.open-meteo.com/v1/forecast?latitude=${city.lat}&longitude=${city.lon}&current_weather=true`,
+		)
+
+		if (!response.ok) {
+			throw new Error("Failed to fetch weather data")
+		}
+
+		const data = await response.json()
+
+		console.log(`${city.name} weather data:`, data)
+
+		return data
+	} catch (error) {
+		console.error("Error:", error)
+	}
+}
+
+async function renderCards() {
+	const container = document.getElementById("cardsContainer")
+
+	for (const key in cities) {
+		const city = cities[key]
+
+		const data = await getWeather(city)
+
+		const temp = data.current_weather.temperature
+		const wind = data.current_weather.windspeed
+
+		const card = document.createElement("div")
+		card.classList.add("card")
+
+		card.innerHTML = `
+                        <h3>${city.name}</h3>
+                        <h4>${city.country}</h4>
+                        <p>${temp}°C</p>
+                        <p>Wind: ${wind} km/h</p>
+                    `
+
+		container.appendChild(card)
+	}
+}
+
+renderCards()
